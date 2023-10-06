b3a0af87f2ef8d20ecbf15984cf2e2516296c415
GODCarNageOP <yashvharale@gmail.com>
2023-10-06 23:48:04 +0530

    bugs fixed

--- src/components/CalendarHeader.jsx
+++ src/components/CalendarHeader.jsx
@@ -2,8 +2,8 @@ import dayjs from "dayjs";
 import React, { useContext } from "react";
 import logo from "../assets/elex.png";
 import GlobalContext from "../context/GlobalContext";
-import {FiChevronLeft} from "react-icons/fi"
-import {FiChevronRight} from "react-icons/fi"
+import {FiChevronLeft} from "react-icons/fi";
+import {FiChevronRight} from "react-icons/fi";
 export default function CalendarHeader() {
   const { monthIndex, setMonthIndex } = useContext(GlobalContext);
   function handlePrevMonth() {
